4a0477c7415d4a1d52f3ee1731f067a12733b2e6
koseki2580 <kingdom_hearts2580@yahoo.co.jp>
2022-05-17 20:39:02 +0900

    深さ優先探索のコンテンツ追加+色々修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/koseki2580/study-docs/edit/tree/main/',
+            'https://github.com/koseki2580/study-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
