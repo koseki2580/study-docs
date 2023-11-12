20a9e187b7dcd4b3d5f45b202f7b18906ea0dcf6
koseki2580 <kingdom_hearts2580@yahoo.co.jp>
2023-11-12 14:14:49 +0900

    /のパスでも表示させる様に変更

--- html/othello/index.js
+++ html/othello/index.js
@@ -1,16 +1,36 @@
 import React from "react";
 import Layout from "@theme/Layout";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+
+const IframeComponent = ({ props }) => {
+  const path = props.pathname;
+  return path.endsWith("/") ? (
+    <iframe
+      title="オセロ"
+      src="/study-docs/html/othello/index.html"
+      style={{ width: "100%", height: "90vh" }}
+    />
+  ) : (
+    <iframe
+      title="オセロ"
+      src="html/othello/index.html"
+      style={{ width: "100%", height: "90vh" }}
+    />
+  );
+};
+
 export default function OthelloComponent() {
   return (
     <Layout
       title="オセロ"
       description="Description will go into a meta tag in <head />"
     >
-      <iframe
-        title="オセロ1"
-        src="html/othello/index.html"
-        style={{ width: "100%", height: "90vh" }}
-      />
+      <Router>
+        <Route
+          path="/study-docs/othello"
+          render={(path) => <IframeComponent props={path.location} />}
+        />
+      </Router>
     </Layout>
   );
 }
