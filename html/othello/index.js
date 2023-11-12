f04e15705cce4b32c02d9254cc29a736f1021fff
koseki2580 <kingdom_hearts2580@yahoo.co.jp>
2023-11-12 14:33:10 +0900

    うまく表示されていなかったので修正

--- html/othello/index.js
+++ html/othello/index.js
@@ -1,39 +1,17 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import BrowserOnly from "@docusaurus/BrowserOnly";
-
-const IframeComponent = ({ props }) => {
-  const path = props.pathname;
-  return path.endsWith("/") ? (
-    <iframe
-      title="オセロ"
-      src="/study-docs/html/othello/index.html"
-      style={{ width: "100%", height: "90vh" }}
-    />
-  ) : (
-    <iframe
-      title="オセロ"
-      src="html/othello/index.html"
-      style={{ width: "100%", height: "90vh" }}
-    />
-  );
-};
 
 export default function OthelloComponent() {
   return (
-    <BrowserOnly>
-      <Layout
+    <Layout
+      title="オセロ"
+      description="Description will go into a meta tag in <head />"
+    >
+      <iframe
         title="オセロ"
-        description="Description will go into a meta tag in <head />"
-      >
-        <Router>
-          <Route
-            path="/study-docs/othello"
-            render={(path) => <IframeComponent props={path.location} />}
-          />
-        </Router>
-      </Layout>
-    </BrowserOnly>
+        src="/study-docs/html/othello/index.html"
+        style={{ width: "100%", height: "90vh" }}
+      />
+    </Layout>
   );
 }
